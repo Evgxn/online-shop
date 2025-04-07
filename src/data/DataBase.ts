2b2676fc3ee64e0a9b8014a68e49e028d5f19784
Evgen <jeka2285311@mail.ru>
2025-04-07 15:34:58 +0300

    add database

---
+++ src/data/DataBase.ts
@@ -0,0 +1,19 @@
+export const SHOP_LIST = [
+  {
+    id: 1,
+    name: "Coca-cola",
+    subTitle: "Описание (1kg, Price)",
+    description: "описание продукта",
+    categoryId: 1,
+    price: 1.99,
+    image: require(""),
+    isFavourite: false,    
+  } 
+];
+
+const CATEGORY_LIST = [
+  {
+    id: 1,
+    title: "Beverages",
+  },
+];
